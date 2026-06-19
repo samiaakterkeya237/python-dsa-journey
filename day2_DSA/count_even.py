266444ed11b8d3828703e5c65a751950e31b185e
samiaakterkeya237 <samiaakterkeya237@gmail.com>
2026-06-19 18:47:23 +0600

    day2 DSA problem solving

---
+++ day2_DSA/count_even.py
@@ -0,0 +1,6 @@
+arr=[1,2,4,7,8]
+count=0
+for x in arr:
+    if x %2==0:
+       count+=1
+print(count)
---
+++ day2_DSA/max.py
@@ -0,0 +1,7 @@
+arr=[5,2,9,1]
+max=arr[0]
+for i in range(len(arr)):
+    if arr[i]>max:
+        max=arr[i]
+
+print(max)
